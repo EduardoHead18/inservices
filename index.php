d13ec457031db912e421e0a61677bd89662798e3
eduardo <kratosedu18@gmail.com>
2022-06-28 13:52:14 -0500

    Eduardo cambio - prueba

--- index.php
+++ index.php
@@ -129,7 +129,7 @@
         <span></span>
         <span></span>
       </button>
-      <a class="navbar-brand text-brand" href="index.php">Inser<span class="color-b">vices</span></a>
+      <a class="navbar-brand text-brand" href="index.php">Inser<span class="color-b">vicesss</span></a>
 
       <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
         <ul class="navbar-nav">
